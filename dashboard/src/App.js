61ef76601ad95f74140b0c52e45a25f466bb1954
unas0706 <109788941+unas0706@users.noreply.github.com>
2026-09-07 18:43:26 +0530

    Make executive console the production app

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+import ExecutiveLedger from './saas-ledger/ExecutiveLedger';
 import SaaSLedger from './saas-ledger';
 import AuthGate from './saas-ledger/AuthGate';
 
-export default function App(){return <Router><Routes><Route path="/" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="/ledger" element={<Navigate to="/" replace/>}/><Route path="*" element={<Navigate to="/" replace/>}/></Routes></Router>}
+export default function App(){return <Router><Routes><Route path="/" element={<AuthGate><ExecutiveLedger/></AuthGate>}/><Route path="/legacy" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="/ledger" element={<Navigate to="/" replace/>}/><Route path="*" element={<Navigate to="/" replace/>}/></Routes></Router>}
