b1ad23b8f48a912daf903080706f49f7dc766712
unas0706 <109788941+unas0706@users.noreply.github.com>
2026-09-07 17:41:06 +0530

    Route SaaS Operating Ledger

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,33 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SaaSLedger from "./saas-ledger";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import { Navigate } from "react-router-dom";
 import ForgotPassword from "./pages/ForgotPassword";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
-  );
-};
-
+const App = () => <Router><Routes>
+  <Route path="/" element={<SaaSLedger />} />
+  <Route path="/ledger" element={<SaaSLedger />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+  <Route path="/forgot-password" element={<ForgotPassword />} />
+</Routes></Router>;
 export default App;
