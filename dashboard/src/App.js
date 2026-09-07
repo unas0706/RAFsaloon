3226afd82d1860f19a34f5fadb3e872e0609ebc0
unas0706 <109788941+unas0706@users.noreply.github.com>
2026-09-07 18:02:46 +0530

    Gate SaaS Ledger behind secure authentication

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,19 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SaaSLedger from "./saas-ledger";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import ForgotPassword from "./pages/ForgotPassword";
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+import SaaSLedger from './saas-ledger';
+import AuthGate from './saas-ledger/AuthGate';
 
-const App = () => <Router><Routes>
-  <Route path="/" element={<SaaSLedger />} />
-  <Route path="/ledger" element={<SaaSLedger />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-  <Route path="/forgot-password" element={<ForgotPassword />} />
-</Routes></Router>;
-export default App;
+export default function App(){return <Router><Routes><Route path="/" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="/ledger" element={<Navigate to="/" replace/>}/><Route path="*" element={<Navigate to="/" replace/>}/></Routes></Router>}
