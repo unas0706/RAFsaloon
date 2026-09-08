b98158c2d2af9714180efafafcce5ebbcfe7412c
unas0706 <109788941+unas0706@users.noreply.github.com>
2026-09-08 18:53:14 +0530

    Make redesigned SaaS Operating Ledger the primary application

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
-import MomentiaApp from './momentia/MomentiaApp';
 import ExecutiveLedger from './saas-ledger/ExecutiveLedger';
 import SaaSLedger from './saas-ledger';
 import AuthGate from './saas-ledger/AuthGate';
 
-export default function App(){return <Router><Routes><Route path="/" element={<AuthGate><MomentiaApp/></AuthGate>}/><Route path="/ledger" element={<AuthGate><ExecutiveLedger/></AuthGate>}/><Route path="/legacy" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="*" element={<Navigate to="/" replace/>}/></Routes></Router>}
+export default function App(){return <Router><Routes><Route path="/" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="/ledger" element={<AuthGate><ExecutiveLedger/></AuthGate>}/><Route path="/legacy" element={<AuthGate><SaaSLedger/></AuthGate>}/><Route path="*" element={<Navigate to="/" replace/>}/></Routes></Router>}
